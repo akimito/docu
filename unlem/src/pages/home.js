4391f4d26467fb764fe06085b88ee130ac1e3055
Akgün Korkmazoğlu <akgun.korkmazoglu@ekupbilisim.com>
2023-12-29 13:25:19 +0300

    first commit

---
+++ unlem/src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function home() {
+  return (
+    <Layout>
+      <h1> home page</h1>
+      <p>This is a Home page</p>
+    </Layout>
+  );
+}
